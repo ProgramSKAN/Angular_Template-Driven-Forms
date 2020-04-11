d1e7cb5a950171e487b6dbae1174b701b5443f5b
SandeepKandagatla <kandagatlas@gmail.com>
2020-04-11 13:58:38 +0530

    SERVER

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { EnrollmentService } from './enrollment.service';
 import { Component } from '@angular/core';
+import { User } from './Models/user';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Angular-Forms';
+  topics = ['Angular', 'React', 'Vue'];
+  topics2 = ['HTML', 'CSS', 'Javascript'];
+  userModel = new User('jack', 'jack@gmail.com', 1234567890, '', 'default', 'morning', true);
+  submitted = false;
+  errorMsg;
+  constructor(private _enrollmentService: EnrollmentService) { }
+
+  topicHasError = true;
+  validateTopic(value) {
+    if (value === 'default') {
+      this.topicHasError = true;
+    } else {
+      this.topicHasError = false;
+    }
+  }
+
+
+  onSubmit(userform) {
+    console.log(userform);//we can use values inside this , or the userModel object that has form changes due to 2 way binding
+
+    this.submitted = true;
+    console.log(this.userModel);
+    this._enrollmentService.enroll(this.userModel)
+      .subscribe(
+        data => console.log('success!', data),
+        error => this.errorMsg = error.statusText
+      )
+
+  }
 }
